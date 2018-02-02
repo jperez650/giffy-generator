0eb5389a7c15abe16ba31f495f19a4e0dd96cb1f
Jose <Perez@Joses-MacBook-Air.local>
2018-02-01 21:36:36 -0800

    finish game logic and add styling to application

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # git-generator
+
+
+One Paragraph of project description goes here
+<img src="/assets/images/poster.jpg">
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+// how to make a local copy of repo
+
+
+### And coding style tests
+
+Explain what these tests test and why
+
+```
+Give an example (3)
+```
+```
+code here
+$.get( queryURL, function(response) {}
+```
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+// open live io link
+// open index.html in browser
+
+## Built With
+
+* [jquery](http://www.dropwizard.io/1.0.2/docs/) - The web framework used
+* [boostrap](https://maven.apache.org/) - Dependency Management
+* [giffy api](https://rometools.github.io/rome/) - Used to generate RSS Feeds
+
+## Author
+
+* **Billie Thompson** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone who's code was used
+* Inspiration
+* etc
+
+
+
--- assets/javascript/giffy.js
+++ assets/javascript/giffy.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 
 
-	var cartoons = ["Dragon Ball", "Captain Tsubasa", "The Simpsons"];
+	var cartoons = ["dragon ball", "captain tsubasa", "the simpsons"];
 
 	function createButton(){
+		$("#buttons-container").empty();
 		for (var i = 0; i < cartoons.length; i++){
 			// console.log(cartoons[i]);
 			var button = $("<button>");
@@ -12,18 +13,90 @@ $(document).ready(function() {
 			$("#buttons-container").prepend(button)
 		}
 	}
-	createButton()
-	// %20
+	createButton();
+
 	$("#buttons-container").on("click", ".choices", function(){
+		$("#pictures-container").empty();
 		//console.log("button clicked");
 		// console.log($(this).text())
 		var selectedButton = $(this).text();
-		console.log(selectedButton);
+		//console.log(selectedButton);
+
+		// base api endpoint url for searching for a string in giphy 
 		var baseURL = "http://api.giphy.com/v1/gifs/search?q="
+		// the limit for the items we are asking for
 		var limit = "&limit=10"
+		/* here we are addind together our baseURL with our search string (seletedButtoon value),the limit to how many items we are getting back from our search and our API key creddentials which we store separately to keep it private */
 		var queryURL = baseURL+selectedButton+limit+apiKey;
-		console.log(queryURL);
-		//http://api.giphy.com/v1/gifs/search?q=cats&api_key=WREXec1W9jT7Dk4N5G3KIH0F3zaTF4Gj
+		//console.log(queryURL);
+		$.get( queryURL, function(response) {
+		  //console.log(response.data);
+		  var results = response.data;
+		  for (var i = 0; i < results.length; i++) {
+		  	// console.log(results[i].rating);
+		  	// console.log(results[i].images.original_still);
+		  	// console.log(results[i].images.original.url);
+		  	var imageWrapper = $("<div>")
+		  	imageWrapper.addClass("wrap");
+		  	var images = $("<img>");
+		  	images.attr("alt", selectedButton+i);
+		  	images.attr("data-still", results[i].images.original_still.url);
+		  	images.attr("data-animate", results[i].images.original.url);
+		  	images.attr("data-state","still");
+		  	images.attr("src", results[i].images.original_still.url);
+		  	var rating = $("<h4>");
+		  	rating.text("rated: "+results[i].rating);
+		  	$(imageWrapper).append(images, rating);
+		  	$("#pictures-container").append(imageWrapper);
+		  }
+
+		});
 	});
+	// create an input and get the value 
+	//event.preventDefault();
+	$("#submit").on("click", function(event){
+		event.preventDefault();
+
+		var newCartoon = ($("#cartoon-input").val().trim());
+		newCartoon = newCartoon.toLowerCase();
+		// console.log(newCartoon)
+		// console.log(cartoons.indexOf(newCartoon));
+		// dont add doubles to array
+		if (cartoons.indexOf(newCartoon) === -1){
+			cartoons.push(newCartoon)
+		}else {
+			$("#alert").text("cartoon already added! try another.");
+		}
+		createButton();
+		$("#cartoon-input").val("");
+	}); 
+
+	$("#pictures-container").on("click", "img", function(){
+		//console.log($(this).attr("data-state"));
+		var currentState = $(this).attr("data-state");
+		console.log(currentState);
+
+		if (currentState === "still") {
+			console.log("helloooo");
+			// change the image souce to be the not still image 
+			$(this).attr("src", $(this).attr("data-animate"));
+			// update the state
+			$(this).attr("data-state", "moving")
+		}
+		else {
+			// console.log("ooo nooooo");
+			$(this).attr("src", $(this).attr("data-still"));
+			// update the state
+			$(this).attr("data-state", "still")
+			// change the image to the still image
+			//$(this)
+			// update the state
+		}
+	});
+	// logic for clicking to animate and make still again
+	// on click get the button we clicked √
+	// get its data-state value √
+	// check if it is moving or still √
+	// change the image accordingly
 
 });
